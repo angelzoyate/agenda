6b5943df2e95f7e166cc6368049667aef2ec756e
angelzoyate <1725110545@utectulancingo.edu.mx>
2026-06-30 17:01:52 +0000

    COMMIT guardar agenda

---
+++ agenda/controllers/ver_contacto.py
@@ -0,0 +1,10 @@
+import web 
+import sqlite3
+
+render = web.template.render ('views', base='layout')
+
+class VerContacto:
+    
+    def GET(self,id_contacto):
+        print(f"ID_CONTACTO: {id_contacto}")
+        return render.ver_contacto()
